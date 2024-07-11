b6589675c4e9bf3f93165bc80c6a4507cadabae6
axelpxl33 <akselnordevpxl33@gmail.com>
2024-07-11 15:31:03 +0300

    Add Aplicatie de gestionare a angajatilor project

---
+++ Aplicatie de gestionare a angajatilor/Aplicatie de gestionare a angajatilor/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicatie_de_gestionare_a_angajatilor
+{
+    internal class Example
+    {
+        static void Main()
+        {
+            FullTimeEmployee exampleFullTimeEmployee = new FullTimeEmployee()
+            {
+                Name = "John Davis",
+                Id = 101,
+                Salary = 3000,
+                Bonus = 5000,
+            };
+            Console.WriteLine($"Full time employee: {exampleFullTimeEmployee.Name}'s annual salary is: {exampleFullTimeEmployee.CalculateAnnualSalary()}");
+
+            PartTimeEmployee examplePartTimeEmployee = new PartTimeEmployee()
+            {
+                Name = "Mark Bark",
+                Id = 105,
+                HourlyRate = 20,
+                HoursWorkedInAWeek = 80,
+            };
+            Console.WriteLine($"Part time employee: {examplePartTimeEmployee.Name}'s annual salary is: {examplePartTimeEmployee.CalculateAnnualSalary()}");
+        }
+        
+    }
+}
---
+++ Aplicatie de gestionare a angajatilor/Aplicatie de gestionare a angajatilor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Employee
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public decimal Salary {  get; set; }
+
+    public virtual decimal CalculateAnnualSalary()
+    {
+        return(Salary * 12);
+    }
+}
+
+class FullTimeEmployee : Employee
+{
+    public decimal Bonus {  get; set; }
+
+    public override decimal CalculateAnnualSalary()
+    {
+        return (Salary * 12) + Bonus;
+    }
+}
+
+class PartTimeEmployee : Employee
+{
+    public decimal HourlyRate {  get; set; }
+    public decimal HoursWorkedInAWeek { get; set; }
+
+    public override decimal CalculateAnnualSalary()
+    {
+        return HourlyRate * HoursWorkedInAWeek * 12;
+    }
+}
