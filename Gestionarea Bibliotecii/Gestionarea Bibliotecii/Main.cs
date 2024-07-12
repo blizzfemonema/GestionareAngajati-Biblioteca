e4b018a87a90da9309470a365d11a00bc75afabe
axelpxl33 <akselnordevpxl33@gmail.com>
2024-07-12 10:26:59 +0300

    Changes commited

--- Gestionarea Bibliotecii/Gestionarea Bibliotecii/Main.cs
+++ Gestionarea Bibliotecii/Gestionarea Bibliotecii/Main.cs
@@ -1,82 +1,134 @@
 ﻿
 using System;
-using System.Collections.Generic;
-
-namespace TheLibraryItself
+public interface IBorrowable
 {
-    public class LibraryItem : IBorrowable
+    void Borrow();
+    void Return();
+    bool IsBorrowed { get; }
+}
+
+public abstract class LibraryItem : IBorrowable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int PublicationYear { get; set; }
+    public bool IsBorrowed { get; private set; }
+
+    public LibraryItem(string title, string author, int publicationYear)
     {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public int PublicationYear { get; set; }
+        Title = title;
+        Author = author;
+        PublicationYear = publicationYear;
+        IsBorrowed = false;
+    }
 
-        public void Borrow()
+    public void Borrow()
+    {
+        if (!IsBorrowed)
         {
-            Console.WriteLine($"{Title} by {Author} borrowed.");
+            IsBorrowed = true;
+            Console.WriteLine($"{Title} has been borrowed.");
         }
-
-        public void Return()
+        else
         {
-            Console.WriteLine($"{Title} by {Author} returned.");
+            Console.WriteLine($"{Title} is already borrowed.");
         }
     }
-    public interface IBorrowable
+
+    public void Return()
     {
-        void Borrow();
-        void Return();
+        if (IsBorrowed)
+        {
+            IsBorrowed = false;
+            Console.WriteLine($"{Title} has been returned.");
+        }
+        else
+        {
+            Console.WriteLine($"{Title} was not borrowed.");
+        }
     }
-    public class Library
+}
+public class Book : LibraryItem
+{
+    public Book(string title, string author, int publicationYear)
+        : base(title, author, publicationYear) { }
+}
+
+public class Magazine : LibraryItem
+{
+    public Magazine(string title, string author, int publicationYear)
+        : base(title, author, publicationYear) { }
+}
+
+public class DVD : LibraryItem
+{
+    public DVD(string title, string author, int publicationYear)
+        : base(title, author, publicationYear) { }
+}
+
+public class Library
+{
+    private List<LibraryItem> items;
+
+    public Library()
     {
-        private List<LibraryItem> items;
+        items = new List<LibraryItem>();
+    }
 
-        public Library()
-        {
-            items = new List<LibraryItem>();
-        }
+    public void AddItem(LibraryItem item)
+    {
+        items.Add(item);
+        Console.WriteLine($"{item.Title} has been added to the library.");
+    }
 
-        public void AddItem(LibraryItem item)
-        {
-            items.Add(item);
-        }
-
-        public void BorrowItem(LibraryItem item)
+    public void BorrowItem(string title)
+    {
+        var item = items.Find(i => i.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        if (item != null)
         {
             item.Borrow();
         }
-
-        public void ReturnItem(LibraryItem item)
+        else
         {
-            item.Return();
-        }
-
-        public void DisplayLibrary()
-        {
-            Console.WriteLine("Library Inventory:");
-            foreach (var item in items)
-            {
-                Console.WriteLine($"- {item.Title} by {item.Author} ({item.PublicationYear})");
-            }
+            Console.WriteLine($"{title} not found in the library.");
         }
     }
 
-    class Program
+    public void ReturnItem(string title)
     {
-        static void Main(string[] args)
+        var item = items.Find(i => i.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        if (item != null)
         {
-            LibraryItem book1 = new LibraryItem { Title = "Book Title 1", Author = "Author A", PublicationYear = 2000 };
-            LibraryItem book2 = new LibraryItem { Title = "Book Title 2", Author = "Author B", PublicationYear = 2010 };
-
-            Library library = new Library();
-            library.AddItem(book1);
-            library.AddItem(book2);
-
-            library.DisplayLibrary();
-
-            library.BorrowItem(book1);
-            library.ReturnItem(book1);
-
-            library.DisplayLibrary();
+            item.Return();
+        }
+        else
+        {
+            Console.WriteLine($"{title} not found in the library.");
         }
     }
 }
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Library library = new Library();
+
+        Book book1 = new Book("The girl on the train", "Paula Hawkins", 2015);
+        Magazine magazine1 = new Magazine("Time", "Henry Luce", 2020);
+        DVD dvd1 = new DVD("Inception", "Christopher Nolan", 2010);
+
+        library.AddItem(book1);
+        library.AddItem(magazine1);
+        library.AddItem(dvd1);
+
+        library.BorrowItem("The girl on the train");
+        library.BorrowItem("The girl on the train");
+
+        library.ReturnItem("The girl on the train");
+        library.ReturnItem("The girl on the train");
+
+        library.BorrowItem("Inception");
+        library.ReturnItem("Time");
+    }
+}
